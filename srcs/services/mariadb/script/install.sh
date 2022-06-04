2b72961b0261c77e9a3d7aaf24cbbf92ac61d551
Benjamin BRASSART <bbrassar@student.42.fr>
2022-06-04 15:57:32 +0200

    Added mysql daemon execution when script is complete

--- srcs/services/mariadb/script/install.sh
+++ srcs/services/mariadb/script/install.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-# TODO check if already installed
+set -e
 
-# https://mariadb.com/kb/en/mysql_install_db/#options
-mysql_install_db								\
-	--auth-root-authentication-method=socket	\
-	--basedir=/usr								\
-	--datadir=/var/lib/mysql					\
-	--skip-test-db								\
-	--user=mysql								\
-	--verbose
+if [ ! -d /run/mysqld ]; then
+	mkdir /run/mysqld
+	chown mysql:mysql /run/mysqld
+fi
+
+printf -- "Installing MariaDB\n"
+if [ -d /var/lib/mysql/mysql ]; then
+	printf -- "MariaDB already installed, skipping\n"
+else
+	# https://mariadb.com/kb/en/mysql_install_db/#options
+	mysql_install_db								\
+		--auth-root-authentication-method=socket	\
+		--basedir=/usr								\
+		--datadir=/var/lib/mysql					\
+		--skip-test-db								\
+		--user=mysql								\
+		--verbose
+fi
+
+exec mysqld --verbose --user=mysql
