80c5bc416044536db1ad2c7e686ba67790b6c5a3
Benjamin BRASSART <bbrassar@student.42.fr>
2022-06-04 15:20:03 +0200

    Added mariadb database installation in script

--- srcs/services/mariadb/script/install.sh
+++ srcs/services/mariadb/script/install.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+# TODO check if already installed
+
+# https://mariadb.com/kb/en/mysql_install_db/#options
+mysql_install_db								\
+	--auth-root-authentication-method=socket	\
+	--basedir=/usr								\
+	--datadir=/var/lib/mysql					\
+	--skip-test-db								\
+	--user=mysql								\
+	--verbose
