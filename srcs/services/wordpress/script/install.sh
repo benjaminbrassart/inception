65cc6674212555c4b36f60b5dd996094860b4282
Benjamin BRASSART <bbrassar@student.42.fr>
2022-06-05 18:31:51 +0200

    Started using wp-cli

--- srcs/services/wordpress/script/install.sh
+++ srcs/services/wordpress/script/install.sh
@@ -1,1 +1,20 @@
-# TODO wp-cli
+#!/bin/sh
+
+WP_PATH='/var/www/html'
+
+set -e
+
+if [ ! -d "$WP_PATH" ]; then
+	wp core download --path="$WP_PATH"
+	cd "$WP_PATH"
+
+	# TODO create config
+	# TODO create db
+	# TODO install
+	# TODO add user accounts
+	# TODO install plugins and themes
+
+	cd -
+fi
+
+exec php-fpm7 -F -y /etc/php7/php-fpm.conf
