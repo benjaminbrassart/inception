a802fce280ef40141b463e058252e19448b7d0a0
Benjamin BRASSART <bbrassar@student.42.fr>
2022-06-13 10:33:59 +0200

    Added ftp virtual user login

--- srcs/services/bonus/ftp/script/run.sh
+++ srcs/services/bonus/ftp/script/run.sh
@@ -1,8 +1,44 @@
 #!/bin/sh
 
+set -x
+
 if [ ! -f /var/log/proftpd/proftpd.log ]; then
 	mkdir -p /var/log/proftpd
 	touch /var/log/proftpd/proftpd.log
 fi
+
+# https://ixnfo.com/en/configuring-proftpd-with-virtual-users-in-a-file.html
+
+#	--passwd
+#		set mode to user password
+#	--force
+#		overwrite passwd file content
+#	--file=
+#		use passwd file instead of default
+#	--name=
+#		name of the user
+#	--uid=
+#		id of the user
+#	--gid=
+#		id of the user's group
+#	--home=
+#		home of the virtual user
+#	--shell=
+#		shell of the virtual user
+#	--stdin
+#		read password from stdin rather that prompting
+ftpasswd \
+	--passwd \
+	--force \
+	--file=/etc/proftpd/ftpd.passwd \
+	--name="$FTP_USER" \
+	--uid=0 \
+	--gid=0 \
+	--home=/srv \
+	--shell=/bin/false \
+	--stdin \
+<< EOF
+$FTP_PASSWORD
+EOF
 
 exec proftpd -n -d10
