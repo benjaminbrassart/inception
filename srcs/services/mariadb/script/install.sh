27963970a0bcf274fbb1d5ad8cd7203fb4de983e
Benjamin BRASSART <bbrassar@student.42.fr>
2022-06-24 15:57:32 +0200

    Fix mariadb install script (hopefully)

--- srcs/services/mariadb/script/install.sh
+++ srcs/services/mariadb/script/install.sh
@@ -20,43 +20,45 @@
 #	--user=mysql
 #		system user account which will be used to perform the installation
 #
-#	--verbose
-#		more information logging
-#
 # see https://mariadb.com/kb/en/mysql_install_db/#options
 #
 MYSQL_INSTALL_OPT="
-	--auth-root-authentication-method=normal
-	--basedir=/usr
-	--datadir=/var/lib/mysql
-	--skip-test-db
-	--user=mysql
+    --auth-root-authentication-method=normal
+    --basedir=/usr
+    --datadir=/var/lib/mysql
+    --skip-test-db
+    --user=mysql
 "
 
 set -e -x
 
 if [ ! -d /run/mysqld ]; then
-	mkdir -p /run/mysqld
-	chown mysql:mysql /run/mysqld
+    mkdir -p /run/mysqld
+    chown mysql:mysql /run/mysqld
 fi
 
 printf -- "Installing MariaDB\n"
 if [ -d /var/lib/mysql/mysql ]; then
-	printf -- "MariaDB already installed, skipping\n"
+    printf -- "MariaDB already installed, skipping\n"
 else
-	# shellcheck disable=2086
-	mariadb-install-db $MYSQL_INSTALL_OPT
+    # shellcheck disable=2086
+    mariadb-install-db ${MYSQL_INSTALL_OPT}
 
-	{
-		# initialize privileges table (disabled when running in bootstrap mode)
-		echo "FLUSH PRIVILEGES;"
-		# change root password
-		echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PASSWORD');"
-		# create new user
-		echo "CREATE USER '$WP_DB_USER'@'%' IDENTIFIED VIA mysql_native_password USING PASSWORD('$WP_DB_PASSWORD');"
-		# give all permissions to the new user
-		echo "GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%';"
-	} | mariadbd --user=mysql --bootstrap
+    # initialize mariadb 'offline' (without a running daemon)
+    {
+        # initialize privileges table (disabled when running in bootstrap mode)
+        echo "FLUSH PRIVILEGES;"
+        # delete all root user except the one with localhost as host
+        echo "DELETE FROM mysql.user WHERE User = 'root' AND Host != 'localhost';"
+        # change root password
+        echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MARIADB_ROOT_PASSWORD}');"
+        # create new user
+        echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';"
+        # give all permissions to the new user
+        echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${WP_DB_USER}'@'%';"
+        # apply modifications to the grant table (maybe not necessary)
+        echo "FLUSH PRIVILEGES;"
+    } | mariadbd --user=mysql --bootstrap
 fi
 
 # delete default configs
