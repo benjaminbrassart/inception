768efe4b00d14af972feb39c24c4d50ae09d33e8
Benjamin BRASSART <bbrassar@student.42.fr>
2022-10-12 04:42:40 +0200

    Add docker installation script

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+PKGS="ca-certificates curl gnupg lsb-release"
+DOCKER_PKGS="docker-ce docker-ce-cli containerd.io docker-compose-plugin"
+
+apt update
+apt install ${PKGS}
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL 'https://download.docker.com/linux/debian/gpg' | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+ARCH="$(dpkg --print-architecture)"
+RELEASE="$(lsb_release -cs)"
+DEB="deb [arch=${ARCH} signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian ${RELEASE} stable"
+
+echo "${DEB}" | tee /etc/apt/sources.list.d/docker.list
+
+apt update
+apt install ${DOCKER_PKGS}
+
+docker run hello-world
